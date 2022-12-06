0d04bef72943c46c6234853a821f6c1b5373fbbb
Xurape <xarope1337@gmail.com>
2022-12-06 15:56:29 +0000

    Adicionadas outras embarcaçõums

--- embarcacoes.c
+++ embarcacoes.c
@@ -28,6 +28,71 @@ void listarEmbarcacoes()
 		}
 	}
 
+	printf("\n\n---------- Cais C ----------");
+	for (size_t i = 1; i <= embarcacoes_id; i++)
+	{
+		if (Dados[i].mat_classe == 3)
+		{
+            printf("\nNúmero -> %d", i);
+			printf("\nNome --> %s", Dados[i].proprietario);
+			printf("\nMatricula --> %s-%d-%d%s", Dados[i].mat_n, Dados[i].mat_num, Dados[i].mat_classe, Dados[i].mat_origem);
+			printf("\nData de entrada --> %d/%d/%d", Dados[i].entrada_dia, Dados[i].entrada_mes, Dados[i].entrada_ano);
+			printf("\nData de saida --> %d/%d/%d\n", Dados[i].saida_dia, Dados[i].saida_mes, Dados[i].saida_ano);
+		}
+	}
+
+	printf("\n\n---------- Cais D ----------");
+	for (size_t i = 1; i <= embarcacoes_id; i++)
+	{
+		if (Dados[i].mat_classe == 4)
+		{
+            printf("\nNúmero -> %d", i);
+			printf("\nNome --> %s", Dados[i].proprietario);
+			printf("\nMatricula --> %s-%d-%d%s", Dados[i].mat_n, Dados[i].mat_num, Dados[i].mat_classe, Dados[i].mat_origem);
+			printf("\nData de entrada --> %d/%d/%d", Dados[i].entrada_dia, Dados[i].entrada_mes, Dados[i].entrada_ano);
+			printf("\nData de saida --> %d/%d/%d\n", Dados[i].saida_dia, Dados[i].saida_mes, Dados[i].saida_ano);
+		}
+	}
+
+	printf("\n\n---------- Cais E ----------");
+	for (size_t i = 1; i <= embarcacoes_id; i++)
+	{
+		if (Dados[i].mat_classe == 5)
+		{
+            printf("\nNúmero -> %d", i);
+			printf("\nNome --> %s", Dados[i].proprietario);
+			printf("\nMatricula --> %s-%d-%d%s", Dados[i].mat_n, Dados[i].mat_num, Dados[i].mat_classe, Dados[i].mat_origem);
+			printf("\nData de entrada --> %d/%d/%d", Dados[i].entrada_dia, Dados[i].entrada_mes, Dados[i].entrada_ano);
+			printf("\nData de saida --> %d/%d/%d\n", Dados[i].saida_dia, Dados[i].saida_mes, Dados[i].saida_ano);
+		}
+	}
+
+	printf("\n\n---------- Cais F ----------");
+	for (size_t i = 1; i <= embarcacoes_id; i++)
+	{
+		if (Dados[i].mat_classe == 6)
+		{
+            printf("\nNúmero -> %d", i);
+			printf("\nNome --> %s", Dados[i].proprietario);
+			printf("\nMatricula --> %s-%d-%d%s", Dados[i].mat_n, Dados[i].mat_num, Dados[i].mat_classe, Dados[i].mat_origem);
+			printf("\nData de entrada --> %d/%d/%d", Dados[i].entrada_dia, Dados[i].entrada_mes, Dados[i].entrada_ano);
+			printf("\nData de saida --> %d/%d/%d\n", Dados[i].saida_dia, Dados[i].saida_mes, Dados[i].saida_ano);
+		}
+	}
+
+	printf("\n\n---------- Cais G ----------");
+	for (size_t i = 1; i <= embarcacoes_id; i++)
+	{
+		if (Dados[i].mat_classe == 7)
+		{
+            printf("\nNúmero -> %d", i);
+			printf("\nNome --> %s", Dados[i].proprietario);
+			printf("\nMatricula --> %s-%d-%d%s", Dados[i].mat_n, Dados[i].mat_num, Dados[i].mat_classe, Dados[i].mat_origem);
+			printf("\nData de entrada --> %d/%d/%d", Dados[i].entrada_dia, Dados[i].entrada_mes, Dados[i].entrada_ano);
+			printf("\nData de saida --> %d/%d/%d\n", Dados[i].saida_dia, Dados[i].saida_mes, Dados[i].saida_ano);
+		}
+	}
+
 	printf("\n1) Voltar para o menu \n2) Sair do programa");
 	printf("\n\n--> ");
 	scanf("%d", &voltar);
